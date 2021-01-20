00cddcf2e19f9d9cf1cf097ab15a9f038936e04f
İbrahim Doğuş <idogus@gmail.com>
2021-01-20 23:51:01 +0300

    20210120 Engin Demirog Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+
+            names.Add("İbrahim");
+            names.Add("Engin");
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
